d5727b67f54f3058eb689c44aa8c0915d79391b3
Vladimir Zorov <vl.zorov@gmail.com>
2022-06-09 22:04:20 +0300

    Add

--- src/StacksAndQueuesLab/SimpleCalculator.java
+++ src/StacksAndQueuesLab/SimpleCalculator.java
@@ -2,19 +2,33 @@ package StacksAndQueuesLab;
 
 
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
 
 public class SimpleCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String [] tokens = scanner.nextLine().split("\\s+");
         ArrayDeque<String> stack = new ArrayDeque<>();
+        String[] symbols = scanner.nextLine().split("\\s+");
+//        Arrays.stream(scanner.nextLine().split(" ")).forEach(stack::push);
+        Collections.addAll(stack,symbols);
 
-        Collections.addAll(stack, tokens);
+        while (stack.size() > 1) {
+            int firstNum = Integer.parseInt(stack.pop());
+            String operator = stack.pop();
+            int secondNum = Integer.parseInt(stack.pop());
 
-        System.out.println();
+            switch (operator) {
+                case "+":
+                    stack.push(String.valueOf(firstNum + secondNum));
+                    break;
+                case "-":
+                    stack.push(String.valueOf(firstNum - secondNum));
+                    break;
+            }
+        }
+            System.out.println(stack.pop());
+
     }
 }
-
-// this is still not ready
