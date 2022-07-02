b99ee781dcebd6d64043a9b9677cfe7d3022fe2c
Vladimir Zorov <vl.zorov@gmail.com>
2022-07-02 16:27:42 +0300

    Add

--- src/JavaAdvanced/MultidimensionalArraysLab/PrintDiagonalsOfSquareMatrix.java
+++ src/JavaAdvanced/MultidimensionalArraysLab/PrintDiagonalsOfSquareMatrix.java
@@ -8,11 +8,23 @@ public class PrintDiagonalsOfSquareMatrix {
         Scanner scanner = new Scanner(System.in);
         int n = Integer.parseInt(scanner.nextLine());
         int matrix [][] = new int[n][n];
+        for (int i = 0; i < n; i++) {
 
-        int[] input = Arrays.stream(scanner.nextLine().split(" "))
-                .mapToInt(Integer::parseInt)
-                .toArray();
+            int[] input = Arrays.stream(scanner.nextLine().split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
 
+            matrix [i] = input;
+        }
+
+        for (int r = 0, c= 0 ; r < n && c < n; r++, c ++) {
+            System.out.print(matrix[r][c] + " ");
+        }
+        System.out.println();
+
+        for (int r = n-1, c= 0 ; r >= 0 && c < n; r--, c ++) {
+            System.out.print(matrix[r][c] + " ");
+        }
 
     }
 }
