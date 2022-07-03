d3678e68d5bde1819f8ee06ca50d62f70e2f2d29
Vladimir Zorov <vl.zorov@gmail.com>
2022-07-03 18:30:20 +0300

    Add

---
+++ src/JavaAdvanced/CarInfo/Car.java
@@ -0,0 +1,36 @@
+package JavaAdvanced.CarInfo;
+
+public class Car {
+    private String brand;
+    private String model;
+    private int horsePower;
+
+    public Car() {
+
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+}
