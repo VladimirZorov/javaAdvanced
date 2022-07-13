8ca11254dccc8f464ed64a539216eb7ddb915387
Vladimir Zorov <vl.zorov@gmail.com>
2022-07-13 21:48:23 +0300

    Add

---
+++ src/JavaOOP/WorkingwithAbstractionExercise/CardRank/CardSuit.java
@@ -0,0 +1,4 @@
+package JavaOOP.WorkingwithAbstractionExercise.CardRank;
+
+public enum CardSuit {
+}
---
+++ src/JavaOOP/WorkingwithAbstractionExercise/CardSuit/CardSuits.java
@@ -0,0 +1,5 @@
+package JavaOOP.WorkingwithAbstractionExercise.CardSuit;
+
+public enum CardSuits {
+    CLUBS, DIAMONDS, HEARTS, SPADES
+}
