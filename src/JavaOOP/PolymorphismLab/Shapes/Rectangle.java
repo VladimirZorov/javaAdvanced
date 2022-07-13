adb2c2137237792b3ce37192e649912615d52fa1
Vladimir Zorov <vl.zorov@gmail.com>
2022-07-13 19:55:19 +0300

    Add

---
+++ src/JavaOOP/PolymorphismLab/Shapes/Rectangle.java
@@ -0,0 +1,25 @@
+package JavaOOP.PolymorphismLab.Shapes;
+
+public class Rectangle extends Shape {
+
+    private Double height;
+
+    private Double width;
+
+    public Rectangle(Double height, Double width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    @Override
+    public Double calculatePerimeter() {
+        return 2 * height + 2 * width;
+    }
+
+    @Override
+    public Double calculateArea() {
+        return height * width;
+    }
+
+
+}
---
+++ src/JavaOOP/PolymorphismLab/Shapes/Shape.java
@@ -0,0 +1,12 @@
+package JavaOOP.PolymorphismLab.Shapes;
+
+public abstract class Shape {
+
+    private Double perimeter;
+    private Double area;
+
+
+    public abstract Double calculatePerimeter();
+    public abstract Double calculateArea();
+
+}
