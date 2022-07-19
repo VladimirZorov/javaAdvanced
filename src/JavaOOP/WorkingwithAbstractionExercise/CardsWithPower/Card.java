34ced0f6dcdfa0a9a66c6280e7b3a71346c1dc50
Vladimir Zorov <vl.zorov@gmail.com>
2022-07-19 22:34:43 +0300

    Add

--- src/JavaOOP/WorkingwithAbstractionExercise/CardsWithPower/Card.java
+++ src/JavaOOP/WorkingwithAbstractionExercise/CardsWithPower/Card.java
@@ -1,4 +1,24 @@
 package JavaOOP.WorkingwithAbstractionExercise.CardsWithPower;
 
 public class Card {
+
+    private CardSuits cardSuits;
+    private CardRanks cardRanks;
+
+    public Card(CardSuits cardSuits, CardRanks cardRanks) {
+        this.cardSuits = cardSuits;
+        this.cardRanks = cardRanks;
+    }
+
+    public CardSuits getCardSuits() {
+        return cardSuits;
+    }
+
+    public CardRanks getCardRanks() {
+        return cardRanks;
+    }
+
+    public int getPower() {
+        return cardRanks.getRankPower() + cardSuits.getSuitPower();
+    }
 }
