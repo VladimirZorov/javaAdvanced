be5ca750ce15a87f48eb19f42114ff21fcb4ef3d
Vladimir Zorov <vl.zorov@gmail.com>
2022-07-19 22:59:16 +0300

    Add

--- src/JavaOOP/WorkingwithAbstractionExercise/CardsWithPower/Card.java
+++ src/JavaOOP/WorkingwithAbstractionExercise/CardsWithPower/Card.java
@@ -21,4 +21,5 @@ public class Card {
     public int getPower() {
         return cardRanks.getRankPower() + cardSuits.getSuitPower();
     }
+
 }
--- src/JavaOOP/WorkingwithAbstractionExercise/CardsWithPower/Main.java
+++ src/JavaOOP/WorkingwithAbstractionExercise/CardsWithPower/Main.java
@@ -1,7 +1,18 @@
 package JavaOOP.WorkingwithAbstractionExercise.CardsWithPower;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        CardRanks cardRanks = CardRanks.valueOf(scanner.nextLine());
+        CardSuits cardSuits = CardSuits.valueOf(scanner.nextLine());
+
+
+        Card card = new Card(cardSuits, cardRanks);
+
+        System.out.printf("Card name: %s of %s; Card power: %d"
+                , card.getCardRanks(), card.getCardSuits(), card.getPower());
     }
 }
