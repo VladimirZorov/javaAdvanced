fe3138dfd5b56106b5b535fffe79e6fab106e61e
Vladimir Zorov <vl.zorov@gmail.com>
2022-07-26 22:42:30 +0300

    Add

--- src/JavaOOP/WorkingwithAbstractionExercise/TrafficLights/Main.java
+++ src/JavaOOP/WorkingwithAbstractionExercise/TrafficLights/Main.java
@@ -1,7 +1,13 @@
 package JavaOOP.WorkingwithAbstractionExercise.TrafficLights;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String [] input = scanner.nextLine().split("\\s+");
+        int numOfTimes;
 
     }
 }
