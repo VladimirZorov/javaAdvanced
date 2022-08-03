22548a0207b643ffb25b02fdb22898b657bbb1cc
Vladimir Zorov <vl.zorov@gmail.com>
2022-08-03 20:42:29 +0300

    Add

--- src/JavaOOP/EncapsulationExercises/ClassBox/Box.java
+++ src/JavaOOP/EncapsulationExercises/ClassBox/Box.java
@@ -1,4 +1,39 @@
 package JavaOOP.EncapsulationExercises.ClassBox;
 
 public class Box {
+
+    private double lenght;
+    private double width;
+    private double height;
+
+    public Box(double length, double width, double height) {
+        this.setLenght(length);
+        this.setWidth(width);
+        this.setHeight(height);
+    }
+
+    public void setLenght(double length) {
+        if (isPositive(length)) {
+            this.lenght = length;
+        }
+    }
+
+    public void setWidth(double width) {
+        if (isPositive(width)) {
+            this.width = width;
+        }
+    }
+
+    public void setHeight(double height) {
+        if (isPositive(height)) {
+            this.height = height;
+        }
+    }
+
+    private boolean isPositive(double number) {
+        if (number <= 0) {
+            throw new IllegalArgumentException("Number must be bigger than zero");
+        }
+        return number > 0;
+    }
 }
